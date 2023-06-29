2632294e4bef3527f4d7b9ffc0e801ad4896b843
Jerric Lyns John <jerriclynsjohn@outlook.com>
2023-06-29 14:09:38 +0530

    fix: reintoduce public URL

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -23,8 +23,11 @@ export async function GET(req: Request) {
 
     if (paymentMethod.methods[0] === undefined) {
       console.log("checkout");
-      const successUrl = new URL("/generate", env.NEXTAUTH_URL).toString();
-      const cancelUrl = new URL("/billing", env.NEXTAUTH_URL).toString();
+      const successUrl = new URL(
+        "/generate",
+        env.NEXT_PUBLIC_APP_URL
+      ).toString();
+      const cancelUrl = new URL("/billing", env.NEXT_PUBLIC_APP_URL).toString();
 
       await tier.updateOrg(`org:${user?.id}`, {
         email: user?.email as string,
@@ -50,7 +53,7 @@ export async function GET(req: Request) {
         console.log(error);
       }
       return new Response(
-        JSON.stringify({ url: new URL("/generate", env.NEXTAUTH_URL) })
+        JSON.stringify({ url: new URL("/generate", env.NEXT_PUBLIC_APP_URL) })
       );
     }
   } catch (error) {
