0e1a3ba9968229b9ae6ed16bb36f42071205caa9
Jerric Lyns John <jerriclynsjohn@outlook.com>
2023-06-29 14:09:38 +0530

    fix: reintoduce public URL

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,5 +17,5 @@ export function formatDate(input: string | number): string {
 }
 
 export function absoluteUrl(path: string) {
-  return `${env.NEXTAUTH_URL}${path}`;
+  return `${env.NEXT_PUBLIC_APP_URL}${path}`;
 }
