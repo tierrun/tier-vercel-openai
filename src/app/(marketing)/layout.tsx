b888b0236148918ea98c6bc391e43ea2abef2038
Jerric Lyns John <jerriclynsjohn@outlook.com>
2023-07-01 13:27:07 +0530

    feat: adding a Github Stargazer button in the marketing header

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -10,9 +10,18 @@ interface MarketingLayoutProps {
 export default async function MarketingLayout({
   children,
 }: MarketingLayoutProps) {
+  const res = await fetch(
+    "https://api.github.com/repos/tierrun/tier-vercel-openai",
+    {
+      method: "GET",
+      next: { revalidate: 60 },
+    }
+  );
+  const data = await res.json();
+
   return (
     <>
-      <Header />
+      <Header count={data.stargazers_count} />
       <main>{children}</main>
       <Footer />
     </>
